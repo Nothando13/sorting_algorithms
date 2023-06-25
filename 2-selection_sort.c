3d65a988c39222023fa9cc043997e037347ddf06
Nothando13 <nphumelele972@gmail.com>
2023-06-25 13:36:32 +0000

    Done

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ *  selection_sort - function that sorts an array of integers in ascending
+ * order using the selection sort algorithm
+ * @size: of the array
+ * @array: list with numbers
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, swap, flag = 0;
+
+	if (i = 0; i < size; i++)
+	{
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)	   
+			print_array(array, size);
+	}
+}
