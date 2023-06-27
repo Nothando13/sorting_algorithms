4f76c8af8d03065c3c8a295751ccc6752bc07a94
Nothando13 <nphumelele972@gmail.com>
2023-06-27 10:47:22 +0000

    Done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,36 @@
 #include "sort.h"
-/** 
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+/**
+ * bubble_sort - Used to sort a given of n elements
+ * provideed in form of an array with n number of elements.
+ * Bubble Sort compares all the element one by one and sort
+ * them based on their values.
+ * @array: Array of data to be sorted
+ * @size: Size of array
  */
+
 void bubble_sort(int *array, size_t size)
 {
+	int i = 0, j = 0, sorted = 1, tmp = 0;
 
-        size_t i, index, tmp = 0;
-
-        if (size < 2)
-                return;
-        for (i = 0; i < size; i++)
-                for (index = 0; index < size; index++)
+	if (size < 2)
+		return;
+	j = size;
+	while (sorted == 1 && j > 0)
+	{
+		sorted = 0;
+		i = 1;
+		while (i <= j)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[i - 1] > array[i] && array[i] != '\0')
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				sorted = 1;
 			}
+			i += 1;
 		}
-}        
+		j -+ 1;
+	}
+}
